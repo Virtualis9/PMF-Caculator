6a4061863f6e767b3f45f6d4c2aa2f97e95d4580
Christopher Peacock <122057441+Virtualis9@users.noreply.github.com>
2024-03-09 10:59:13 +0000

    Bug fixes update

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -24,13 +24,19 @@ function calculateValue(inputElementById, multiplyer, outputElementById) {
 //output percenatge
 function serviceLaying(inputElementById1, multiply1, inputElementById2, multipler2, outputElementById) {
     let input1 = document.getElementById(inputElementById1).value
+    console.log(input1)
     let times1 = multiply1
+    console.log(times1)
     let input2 = document.getElementById(inputElementById2).value
+    console.log(input2)
     let times2 = multipler2
+    console.log(times2)
     let result = input1 * times1 + input2 * times2;
+    console.log(result)
     document.getElementById(outputElementById).textContent = `${result}%`;
-    console.log(document.getElementById(outputElementById).textContent)
+    console.log(document.getElementById(outputElementById).textContent = `${result}%`)
     updateHistory(outputElementById, result);
+
 }
 
 function toggleDropdown(mainSubMenuId) {
